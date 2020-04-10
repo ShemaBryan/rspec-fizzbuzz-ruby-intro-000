1ab474f7169c71846a2146b1b78058f05745ad77
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-10 00:09:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,11 @@
 require_relative './spec_helper.rb'
   def fizzbuzz(int)
-    if int % 3 == 0 
-      "Fizz"
+  if number % 3 == 0 && int % 5 == 0
+   puts "FizzBuzz"
+
+
+
+    if int % 3 == 0
+    puts  "Fizz"
     end
 end
